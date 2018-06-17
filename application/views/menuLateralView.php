ca3efd0a910e87b77b9f88ee03aea64e8cdec1fe
jdetecnologias <jdetecnologias@gmail.com>
2018-06-17 07:51:36 -0400

    HI

---
+++ application/views/menuLateralView.php
@@ -0,0 +1,39 @@
+<div class="col-md-3 mx-0 " id="div-barra-lateral">
+			<nav class="navbar navbar-fixed-top border " id="nav-navbar-lateral"><!--navbar lateral -->
+				<ul id="ul-menu-lateral" class="navbar-nav " ><!--ul-lateral -->
+					<li><a class="borda btn btn-outline-primary" style="text-align:left;" href="<?php echo base_url();?>"><i class="fas fa-home fa-lg "></i><span role="separator"></span> Home</a></li>
+					<span class="divisor2"></span>
+					<li>
+						<a class="borda btn btn-outline-primary " style="text-align:left;"  href="<?php echo base_url("exibir/clientes");?>">
+							<i class="fas fa-users fa-lg "></i>
+							<span role="separator" class=""></span>Clientes 
+						</a>
+					</li>
+					<span class="divisor2"></span>
+					<li>
+						<a class="borda btn btn-outline-primary" style="text-align:left;"  href="<?php echo base_url("anotarPedido/formulario");?>">
+								<i class="fas fa-cart-plus fa-lg"></i>
+								<span role="separator"></span> Vendas
+						</a>
+					</li>
+					<span class="divisor2"></span>	
+          	<li>
+						<a class="borda btn btn-outline-primary" style="text-align:left;"  href="<?php echo base_url("cadastrarProduto");?>">
+								<i class="fas fa-cart-plus fa-lg"></i>
+								<span role="separator"></span> Produtos
+						</a>
+					</li>
+					<span class="divisor2"></span>	
+          	<li>
+						<a class="borda btn btn-outline-primary" style="text-align:left;"  href="<?php echo base_url("VerPedido");?>">
+								<i class="fas fa-cart-plus fa-lg"></i>
+								<span role="separator"></span>Perdidos
+						</a>
+					</li>
+					<span class="divisor2"></span>
+        
+				</ul><!--/ul-lateral -->
+			</nav><!--/navbar lateral -->
+		</div><!--/div-barra-lateral -->
+
+<!-- Menu -->
